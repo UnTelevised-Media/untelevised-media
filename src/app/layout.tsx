3ff4df17d062c5362740a256029db878b4c94ba9
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-08-10 19:47:01 -0500

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,24 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'UnTelevised Media',
   description: 'The Revolution will be UnTelevised',
-}
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  authors: [{ name: 'Radical Edward' }, { name: 'Digitl Alchemyst', url: 'https://digitl-alchemyst-portfolio.vercel.app/' }],
+  colorScheme: 'dark',
+  creator: 'Radical Edward',
+  publisher: 'Digitl Alchemyst',
+  icons: {
+    icon: '/favicon.ico'
+  }
+
+  }
 
 export default function RootLayout({
   children,
@@ -16,7 +27,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+        <html lang="en">
+    {/* <Head>
+        <link rel='icon' href='/favicon.ico'/>
+      </Head> */}
       <body className={inter.className}>{children}</body>
     </html>
   )
