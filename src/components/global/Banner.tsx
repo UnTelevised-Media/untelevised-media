87e5ee4ec8c9c638b4d5916d49ab446c2e48b80a
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-08-19 16:06:11 -0500

    word

--- src/components/global/Banner.tsx
+++ src/components/global/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
             <h1 className='text-untele text-7xl font-bold'>UnTelevised Media</h1>
             <h2 className='mt-5 md:mt-0'>The Revolution will be {' '}<span className='underline decoration-2 decoration-untele font-semibold'>UnTelevised</span></h2>
         </div>
-        <p className='mt-5 md:mt-2 text-untele max-w-md lg:max-w-lg'>The latest breaking news you wont hear on mainstream media</p> 
+        <p className='mt-5 md:mt-2 text-untele max-w-md lg:max-w-lg'>The latest breaking news you wont see on mainstream media</p> 
     </div>
   )
 }
