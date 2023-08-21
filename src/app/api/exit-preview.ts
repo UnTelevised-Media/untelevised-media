c7564fb47b87ff7d6ca44458e84be8a95c619dec
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-08-21 17:50:12 -0500

    Clean up

--- src/app/api/exit-preview.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function exit(req: NextApiRequest, res: NextApiResponse) {
-  res.clearDraftMode();
-  res.writeHead(307, { Location: '/' });
-  res.end();
-}
-
-
