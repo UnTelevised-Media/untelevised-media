07bded9c85a2c877c32d1a2c8cae235f558bcf82
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-08-22 22:21:53 -0500

    Fetching Data to Dynamic Pages

--- src/app/(user)/post/[slug]/page.tsx
+++ src/app/(user)/post/[slug]/page.tsx
@@ -1,3 +1,28 @@
 /* eslint-disable react/function-component-definition */
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>}
+import { client } from '@/lib/sanity.client';
+import { groq } from 'next-sanity';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+async function Article({ params: { slug } }: Props) {
+  
+const query = groq`
+  *[_type == "post" && slug.current == $slug][0] {
+        ...,
+        author->,
+        categories[]->,
+        'comments': *[
+          _type == 'comment' &&
+          post._ref == ^._id &&
+          approved == true],
+    }`;
+
+  const article: Post = await client.fetch(query, { slug });
+  
+
+  return <div>My Post: {slug}</div>}
+
+  export default Article
---
+++ structure.ts
@@ -0,0 +1,42 @@
+/* eslint-disable import/prefer-default-export */
+import { Iframe } from 'sanity-plugin-iframe-pane';
+import type { DefaultDocumentNodeResolver } from 'sanity/desk';
+// import { SanityDocument } from 'sanity';
+
+// Customise this function to show the correct URL based on the current document
+// function getPreviewUrl(post: SanityDocument) {
+//   return post?.slug?.current
+//     ? `${window.location.host}/${post.slug?.current}`
+//     : `${window.location.host}`
+// }
+
+export const getDefaultDocumentNode: DefaultDocumentNodeResolver = (
+    S,
+    { schemaType }
+) => {
+   if (schemaType === 'post') {
+  
+           return S.document().views([
+             S.view.form(),
+             S.view
+               .component(Iframe)
+               .options({
+                 url: `${
+                    process.env.NEXT_PUBLIC_VERCEL_URL ||
+                    'http://localhost.3000'
+                    }/api/preview`,
+                    defaultSize: `desktop`, // default `desktop`
+                    reload: {
+                        button: true,
+                    },
+                    loader: true,
+                    attributes: {
+                      allow: 'fullscreen', // string, optional
+                    //   referrerPolicy: 'strict-origin-when-cross-origin', // string, optional
+                    //   sandbox: 'allow-same-origin', // string, optional
+                    }
+               })
+               .title('Preview'),
+           ]);
+       }
+     };
