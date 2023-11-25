69cbbca70d94b5d7d8594ece51f2603d527a62d3
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2023-11-25 13:51:20 -0600

    Ticker Date Sorting Fixxed

--- src/components/global/Ticker.tsx
+++ src/components/global/Ticker.tsx
@@ -5,60 +5,61 @@ import { client } from '@/lib/sanity.client';
 const queryPost = groq`
   *[_type=='post'] {
     ...,
+    slug,
     eventDate,
+    publishedAt,
     title,
   } 
   | order(_createdAt desc)
 `;
 
-const queryLiveEvent = groq`
-  *[_type=='liveEvent'] {
+const queryKeyEvent = groq`
+  *[_type=='keyEvent'] {
     ...,
     slug,
-    keyEvent[]->{
-      title,
-      eventDate,
-    },
-    relatedArticles[]-> {
-      slug,
-      _id,
-      title,
-      eventDate,
-    }
+    eventDate,
+    title,
   } 
 `;
 
 export default async function Ticker() {
-  const posts = await client.fetch(queryPost);
-  const liveEvents = await client.fetch(queryLiveEvent);
+  try {
+    const posts = await client.fetch(queryPost);
+    const keyEvents = await client.fetch(queryKeyEvent);
 
-  const keyEventTitles = liveEvents.reduce((titles, event) => {
-    if (event.keyEvent) {
-      const keyEventTitles = event.keyEvent.map((keyEvent) => keyEvent.title);
-      titles.push(...keyEventTitles);
-    }
-    return titles;
-  }, []);
+    const keyEventTitles = keyEvents.map((keyEvent) => ({
+      title: keyEvent.title,
+      date: keyEvent.eventDate || keyEvent.publishedAt,
+    }));
 
-  const postTitles = posts.map((post) => post.title);
+    const postTitles = posts.map((post) => ({
+      title: post.title,
+      date: post.eventDate || post.publishedAt,
+    }));
 
-  const allTitles = [...keyEventTitles, ...postTitles].sort(
-    (a, b) =>
-      new Date(b.eventDate || b.publishedAt).getTime() -
-      new Date(a.eventDate || a.publishedAt).getTime(),
-  );
+    const allTitles = [...keyEventTitles, ...postTitles].sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+      );
+      console.log("🚀 ~ file: Ticker.tsx:41 ~ Ticker ~ allTitles:", allTitles)
 
-  return (
-    <div className='flex w-full overflow-hidden rounded-md border border-untele/30 bg-static shadow-lg'>
-      <div className='marquee flex h-12 items-center justify-center '>
-        <div className='track'>
-          <div className='text-3xl font-bold'>
-            {allTitles.map((title, index) => (
-              <span key={index}>{title} • </span>
-            ))}
+    // Return JSX here, inside the try block
+    return (
+      <div className='flex w-full overflow-hidden rounded-md border border-untele/30 bg-static shadow-lg'>
+        <div className='marquee flex h-12 items-center justify-center '>
+          <div className='track'>
+            <div className='text-3xl font-bold'>
+              {allTitles.map((title, index) => (
+                <span key={index}>
+                  {title.title} •{' '}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
 }
