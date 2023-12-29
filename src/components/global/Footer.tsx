e309d3eba2d88797df8a047287375ea6cdecde51
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2023-12-28 23:27:17 -0600

    Footer Layout

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,33 +1,71 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
   return (
     // Footer
-    <div className='bg-slate-600 flex flex-col'>
-      {/* News Sections & About  */}
-      <div className='bg-slate-600 flex flex-row'>
-        {/* Section  */}
-        <div className='bg-slate-600'>.</div>
-        {/* Section  */}
-        <div className='bg-slate-600'>.</div>
-        {/* Media  */}
-        <div className='bg-slate-600'>.</div>
-        {/* About  */}
-        <div className='bg-slate-600'>.</div>
+    <div className='flex flex-col space-y-10 bg-slate-600 px-2 py-3'>
+      <div className='flex flex-row space-x-6'>
+        {/* News Sections & About  */}
+        <div className='flex flex-col'>
+          <p className='text-slate-950'>News Categories</p>
+        </div>
+
+        {/* Media */}
+        <div className='flex flex-col'>
+          <p className='text-slate-950'>Media</p>
+          <Link href='/'>Photo</Link>
+          <Link href='/'>Video</Link>
+          <Link href='/'>Investigations</Link>
+          <Link href='/'>RSS</Link>
+        </div>
+        {/* Social Links */}
+        <div className='flex flex-col'>
+          <p className='text-slate-950'>Social Links</p>
+          <Link href='/'>Youtube</Link>
+          <Link href='/'>Twitch</Link>
+          <Link href='/'>TikTok</Link>
+          <Link href='/'>Twitter/X</Link>
+          <Link href='/'>Threads</Link>
+          <Link href='/'>Facebook</Link>
+          <Link href='/'>Instagram</Link>
+          <Link href='/'>Reddit</Link>
+          <Link href='/'>Discord</Link>
+          <Link href='/'>D-Live</Link>
+          <Link href='/'>Kick</Link>
+        </div>
+        {/* Principles & Policies  */}
+        <div className='flex flex-col'>
+          <p className='text-slate-950'>Principles & Policies</p>
+          <Link href='/'>Terms of Service</Link>
+          <Link href='/'>Licensing Terms</Link>
+          <Link href='/'>Privacy Policy</Link>
+          <Link href='/'>Cookie Settings</Link>
+          <Link href='/'>RSS Terms of Service</Link>
+          <Link href='/'>Ad Choices</Link>
+          <Link href='/'>Discussion Policy</Link>
+          <Link href='/'>Submissions Policy</Link>
+        </div>
+        {/* About */}
+        <div className='flex flex-col'>
+          <p className='text-slate-950'>About</p>
+          <Link href='/'>About UnTelevised</Link>
+          <Link href='/'>Meet Our Staff</Link>
+          <Link href='/'>Join Our Team</Link>
+          <Link href='/'>Contact the Newsroom</Link>
+          <Link href='/'>Licensing & Syndication</Link>
+          <Link href='/'>Advertise</Link>
+          <Link href='/'>Send a News Tip</Link>
+          <Link href='/'>Request a Correction</Link>
+        </div>
       </div>
-      {/* Principles & Social  */}
-      <div className='bg-slate-600 flex flex-row'>
-        {/* Principles & Ethics  */}
-        <div className='bg-slate-600'>.</div>
-        {/* Social Links  */}
-        <div className='bg-slate-600'>.</div>
-      </div>
-      {/* Policies & Copywrite  */}
-      <div className='bg-slate-600 flex flex-col'>
-        {/* Policies  */}
-        <div className='bg-slate-600'>.</div>
+
+      {/* Copywrite Notice */}
+      <div className='flex'>
         {/* Copywrite  */}
-        <div className='bg-slate-600'>.</div>
+        <p className=''>
+          © Copyright 2023 UnTelevised Media™. All Rights Reserved.
+        </p>
       </div>
     </div>
   );
